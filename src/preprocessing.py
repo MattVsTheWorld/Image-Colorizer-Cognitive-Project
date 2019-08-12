8589cbfe68934b97ba7a8ac7a419be5ea3c83b0f
Matteo <matteo.marcuzzo.it@gmail.com>
2019-08-12 18:32:09 +0200

    folder loader

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,6 +6,7 @@ from tensorflow import keras
 import numpy as np
 import os
 import cv2
+from glob import glob
 
 
 class ImageLoader:
@@ -16,13 +17,26 @@ class ImageLoader:
         img_color = cv2.cvtColor(cv2.imread(path, cv2.IMREAD_COLOR),
                                  cv2.COLOR_BGR2Lab)
 
+        img_color = cv2.resize(img_color, (self.img_size, self.img_size))
         img_bw = img_color.copy()
         for i in range(0, self.img_size):
             for j in range(0, self.img_size):
                 img_bw[i][j] = img_bw[i][j].take(1)
         return img_color, img_bw
 
+    def load_folder(self, folder_path):
+        image_list = []
+        for filename in glob(folder_path + '/*.jpg'):
+            image_list.append(self.load_img(filename))
+        return image_list
 
+#
+# from PIL import Image
+# import glob
+# image_list = []
+# for filename in glob.glob('yourpath/*.gif'): #assuming gif
+#     im=Image.open(filename)
+#     image_list.append(im)
 def main():
 
     # Allows INFO logs to be printed
@@ -31,11 +45,19 @@ def main():
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
     loader = ImageLoader(256)
-    imgc, imgbw = loader.load_img(os.pardir + '/imagenet/a_subfolder/twitch.jpg')
-    cv2.imshow('img', imgc)
-    cv2.waitKey()
-    cv2.imshow('img', imgbw)
-    cv2.waitKey()
+    # imgc, imgbw = loader.load_img(os.pardir + '/test_imgs/a_subfolder/twitch.jpg')
+    # cv2.imshow('img', imgc)
+    # cv2.waitKey()
+    # cv2.imshow('img', imgbw)
+    # cv2.waitKey()
+    # list = loader.load_folder(os.pardir + '/test_imgs/a_subfolder')
+    # for (imgc, imgbw) in list:
+    #     cv2.imshow('img', imgc)
+    #     cv2.waitKey()
+    #     cv2.imshow('img', imgbw)
+    #     cv2.waitKey()
+
+
 
 
 if __name__ == '__main__':
