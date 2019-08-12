7880c60577641ab83be81afff22b8c3fcd5b3480
Matteo <matteo.marcuzzo.it@gmail.com>
2019-08-12 16:11:38 +0200

    DELETOS

---
+++ src/preprocessing.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import os
+import cv2
+
+# Allows INFO logs to be printed
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+# Set verbosity of warnings allowed (higher number, less warnings)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+img = cv2.cvtColor(cv2.imread(os.pardir + '/imagenet/a_subfolder/twitch.jpg', cv2.IMREAD_COLOR), cv2.COLOR_BGR2Lab)
+
+imgBW = img.copy()
+for i in range(0, 256):
+    imgBW[i] = imgBW[i].take(1)
+
+cv2.imshow('img', imgBW)
+cv2.waitKey()
