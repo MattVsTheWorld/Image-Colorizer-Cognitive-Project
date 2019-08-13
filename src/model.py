63aebb8336190e4f509d22c9a997b4df47c1e504
Matteo <matteo.marcuzzo.it@gmail.com>
2019-08-13 12:12:11 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,15 +4,16 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import os
+import cv2
 from src.preprocessing import ImageLoader
 
 
 def loss_l3(found, truth):
-    return tf.divide((tf.pow(tf.subtract(truth, found), 3)), 3)
+    return tf.divide((tf.pow(tf.abs(tf.subtract(truth, found)), 3)), 3)
 
 
 def main():
-    IMG_SIZE = 28
+    IMG_SIZE = 256
     # Allows INFO logs to be printed
     tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
     # Set verbosity of warnings allowed (higher number, less warnings)
@@ -27,16 +28,25 @@ def main():
     # train_labels = train_labels.reshape((-1, 256, 256, 3))
 
     model = keras.Sequential()
-    model.add(keras.layers.Conv2D(16, (3, 3), padding='same', activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 1)))
-    # model.add(keras.layers.MaxPooling2D((2, 2)))
-    # model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 1)))
+    model.add(keras.layers.Conv2D(16, (3, 3), padding='same', activation='relu'))
+    model.add(keras.layers.Conv2D(3, (3, 3), padding='same', activation='relu'))
     # model.add(keras.layers.MaxPooling2D((2, 2)))
     # model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
 
     # model.summary()
     model.compile(optimizer='adam',
                   loss=loss_l3)
-    model.fit(train_images, train_labels, epochs=5)
+    model.fit(train_images, train_labels, epochs=200)
+
+    img, kappa = loader.load_img(os.pardir + '/test_imgs/a_subfolder/twitch.jpg')
+    cv2.imshow('lul', cv2.cvtColor(kappa, cv2.COLOR_Lab2BGR))
+    cv2.waitKey()
+    img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=3)
+    result = model.predict(img)
+    cv2.imshow('lul', cv2.cvtColor(result[0], cv2.COLOR_Lab2BGR))
+    cv2.waitKey()
 
 
 if __name__ == '__main__':
