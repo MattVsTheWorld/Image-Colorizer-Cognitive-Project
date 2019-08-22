87dc120da8e747a4f3c50427200a063a5838a059
Matteo <matteo.marcuzzo.it@gmail.com>
2019-08-22 16:46:12 +0200

    Giovanni gay capo degli ebrei

---
+++ src/config.py
@@ -0,0 +1,3 @@
+percentage_training = 0.99
+batch_size = 32
+img_rows, img_cols = 128, 128
---
+++ src/data_generator.py
@@ -0,0 +1,165 @@
+from numpy.core._multiarray_umath import ndarray
+from typing import List, Tuple
+import os
+import random
+from random import shuffle
+import cv2
+import numpy as np
+import sklearn.neighbors as nn
+from keras.utils import Sequence
+from src.config import percentage_training, batch_size, img_rows, img_cols
+
+
+def get_soft_encoding(image_ab, nn_finder, num_q) -> ndarray:
+    height: int
+    width: int
+    # take shape of first two
+    height, width = image_ab.shape[:2]
+
+    # flatten ndarray of the two channels
+    a: ndarray = np.ravel(image_ab[:, :, 0])
+    b: ndarray = np.ravel(image_ab[:, :, 1])
+    # Metti a fianco
+    ab: ndarray = np.vstack((a, b)).T
+    # Get the distance to and the idx of the nearest neighbors (of the color gamut)
+    dist_neigh, idx_neigh = nn_finder.kneighbors(ab)
+    # Smooooooth weights with gaussian kernel
+    sigma: int = 5
+    weights = np.exp(-dist_neigh ** 2 / (2 * sigma ** 2))
+    weights = weights / np.sum(weights, axis=1)[:, np.newaxis]
+    # ----
+    # Reshape y
+    # shape[0] is length of one of the channels (a)
+    y: ndarray = np.zeros((ab.shape[0], num_q))
+    # create indexes from 0 to ab.shape[0]
+    # put them in an array with a new axis added
+    idx_pts = np.arange(ab.shape[0])[:, np.newaxis]
+    # give each color a weight corresponding to how far are its closest 5 neighbours
+    y[idx_pts, idx_neigh] = weights
+    # num_q (313) as third shape; only num_neighbours (5) will have a non-0 value
+    y = np.reshape(y, (height, width, num_q))
+    return y
+
+
+class DataGenSequence(Sequence):
+    def __init__(self, usage, image_folder):
+        # Train or validation
+        self.usage: str = usage
+        self.image_folder = image_folder
+
+        if usage == 'train':
+            names_file: str = 'image_names/train_names.txt'
+        else:
+            names_file: str = 'image_names/valid_names.txt'
+
+        with open(names_file, 'r') as f:
+            self.names: List[str] = f.read().splitlines()
+
+        # np.random.shuffle(self.names)
+
+        # Load the array of quantized ab value
+        q_ab: ndarray(dtype=int, shape=(313, 2)) = np.load("data/lab_gamut.npy")
+        self.num_q: int = q_ab.shape[0]
+
+        # Fit a NN to q_ab
+        self.nn_finder = nn.NearestNeighbors(algorithm='ball_tree').fit(q_ab)
+
+    def __len__(self) -> int:
+        # Number of batches
+        return int(np.ceil(len(self.names) / float(batch_size)))
+
+    def __getitem__(self, idx: int) -> Tuple[ndarray, ndarray]:
+        """
+        Return an ndarray containing the images contained in batch n°idx
+        :param idx: index of the batch
+        :return: ndarray containing the batch
+        """
+        # First element of the batch
+        i: int = idx * batch_size
+
+        art: int = 4
+        out_img_rows: int
+        out_img_cols: int
+        out_img_rows, out_img_cols = img_rows // art, img_cols // art
+        # Batch is either full or partial (last batch)
+        length: int = min(batch_size, (len(self.names) - i))
+
+        # e.g. shape= (32, 256, 256, 1)
+        batch_x: ndarray = np.empty((length, img_rows, img_cols, 1), dtype=np.float32)
+        # e.g. shape= (32, 64, 64, 313)
+        batch_y: ndarray = np.empty((length, out_img_rows, out_img_cols, self.num_q), dtype=np.float32)
+
+        for i_batch in range(length):
+            name: str = self.names[i]
+            filename: str = os.path.join(self.image_folder, name)
+            # b: 0 <=b<=255, g: 0 <=g<=255, r: 0 <=r<=255.
+            bgr: ndarray = cv2.resize(cv2.imread(filename), (img_rows, img_cols))
+            gray: ndarray = cv2.resize(cv2.imread(filename, cv2.IMREAD_GRAYSCALE), (img_rows, img_cols))
+            lab: ndarray = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+            # Normalize
+            x: ndarray = gray / 255.
+
+            out_lab: ndarray = cv2.resize(lab, (out_img_rows, out_img_cols), cv2.INTER_AREA)
+            # rows, columns, L a b; skip L
+            out_ab: ndarray = out_lab[:, :, 1:].astype(np.int32) - 128
+
+            y: ndarray = get_soft_encoding(out_ab, self.nn_finder, self.num_q)
+
+            # if np.random.random_sample() > 0.5:
+            #     # x is gray normalized
+            #     x = np.fliplr(x)
+            #     y = np.fliplr(y)
+
+            # populate batches
+            batch_x[i_batch, :, :, 0] = x
+            batch_y[i_batch] = y
+
+            i += 1
+
+        return batch_x, batch_y
+
+    def on_epoch_end(self):
+        np.random.shuffle(self.names)
+
+
+def train_gen(image_folder: str) -> DataGenSequence:
+    return DataGenSequence('train', image_folder)
+
+
+def valid_gen(image_folder: str) -> DataGenSequence:
+    return DataGenSequence('valid', image_folder)
+
+
+def split_data(image_folder: str, fmt: str):
+    names: List[str] = [f for f in os.listdir(image_folder) if f.lower().endswith(fmt)]
+    # Number of samples
+    num_samples: int = len(names)           # 2601
+    print('num_samples: ' + str(num_samples))
+
+    # Number of train/validation images
+    num_train_samples: int = int(num_samples * percentage_training)
+    print('num_train_samples: ' + str(num_train_samples))
+    num_valid_samples: int = num_samples - num_train_samples
+    print('num_valid_samples: ' + str(num_valid_samples))
+
+    # Pick random validation file names
+    valid_names = random.sample(names, num_valid_samples)
+    train_names = [n for n in names if n not in valid_names]
+    # shuffle(valid_names)
+    # shuffle(train_names)
+
+    with open('image_names/valid_names.txt', 'w') as file:
+        file.write('\n'.join(valid_names))
+
+    with open('image_names/train_names.txt', 'w') as file:
+        file.write('\n'.join(train_names))
+
+
+def main():
+    image_folder: str = os.pardir + 'test_imgs/bird'
+    fmt: str = '.jpeg'
+    split_data(image_folder, fmt)
+
+
+if __name__ == '__main__':
+    main()
